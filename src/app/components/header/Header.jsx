ff68bd4b0195b2e3c59cdf68908f48e50eca8cea
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-07-30 11:52:07 +0300

    Sign In , sign Up and user Management tested

---
+++ src/app/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './Header.module.css'
+import Link from 'next/link'
+import { auth } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
+function Header() {
+  const {userId} = auth();
+  return (
+    <>
+    <div className={styles.nav} >
+    <div className={styles.brand}>
+        Clerk Authentication
+    </div>
+
+    {  !userId && <button className={styles.signInBtn}> 
+    <Link href="/sign-up" className={styles.link}>SignUp</Link>
+    </button>}
+    { !userId &&<button className={styles.signUpBtn}>
+    <Link href="/sign-up" className={styles.link}>SignUp</Link>
+    </button>}
+    <UserButton/>
+    </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+
+function SignUpPage() {
+  return (
+    <>
+
+    <SignUp/>
+      
+    </>
+  )
+}
+
+export default SignUpPage
